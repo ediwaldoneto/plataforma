e9c11357e5998dad877b5640fc1ebe2b5bbe7079
ediwaldoneto <ediwaldo.neto@outlook.com>
2019-12-09 07:21:17 -0300

    Integrando EJS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('BEM VINDO');
+  res.render('home');
 });
 
 app.listen(8081, () => {
