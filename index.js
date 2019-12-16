fc35ede48a19e274625f2874c13f51dcb1fc521b
ediwaldoneto <ediwaldo.neto@outlook.com>
2019-12-16 07:48:30 -0300

    criando formulario de perguntas

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.render('home');
 });
 
+app.get('/question', (req, res) => {
+  res.render('question');
+});
+
 app.listen(8081, () => {
   console.log('Servico em execucao');
 });
